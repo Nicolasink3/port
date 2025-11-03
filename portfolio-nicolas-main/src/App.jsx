dad94caaca0bb02b8c87dd50112891c7fe34721a
Nicolasink3 <nicolas.r.p2018@gmail.com>
2025-11-03 09:40:05 -0300

    Add files via upload

---
+++ portfolio-nicolas-main/src/App.jsx
@@ -0,0 +1,38 @@
+import styles from './App.module.css'
+
+function App() {
+
+  //javascript
+
+  return (
+    <>
+      <nav className={styles.menu}>
+        <a href="#s1">Home</a>
+        <a href="#s2">Cards</a>
+        <a href="#s3">Video</a>
+        <a href="#s4">Contato</a>
+      </nav>
+      <main>
+
+        <section>
+          <h2>sessao 1</h2>
+        </section>
+
+        <section>
+          <h2>sessao 2</h2>
+        </section>
+
+        <section>
+          <h2>sessao 3</h2>
+        </section>
+
+        <section>
+          <h2>sessao 4</h2>
+        </section>
+
+      </main>
+    </>
+  )
+}
+
+export default App
